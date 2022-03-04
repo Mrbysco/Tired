893b4e7bb2280d296112943de8f5d8d4f0c616bc
Mrbysco <Mrbysco@users.noreply.github.com>
2022-03-04 03:26:26 +0100

    Update forge

--- src/main/java/com/mrbysco/tired/Tired.java
+++ src/main/java/com/mrbysco/tired/Tired.java
@@ -1,5 +1,6 @@
 package com.mrbysco.tired;
 
+import com.mojang.logging.LogUtils;
 import com.mrbysco.tired.handler.SleepHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -7,19 +8,18 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 
 @Mod(Tired.MOD_ID)
 public class Tired {
-    public static final String MOD_ID = "tired";
-    public static final Logger LOGGER = LogManager.getLogger();
+	public static final String MOD_ID = "tired";
+	public static final Logger LOGGER = LogUtils.getLogger();
 
-    public Tired() {
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        ModLoadingContext.get().registerConfig(Type.COMMON, SleepSchedule.commonSpec);
-        eventBus.register(SleepSchedule.class);
+	public Tired() {
+		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		ModLoadingContext.get().registerConfig(Type.COMMON, SleepSchedule.commonSpec);
+		eventBus.register(SleepSchedule.class);
 
-        MinecraftForge.EVENT_BUS.register(new SleepHandler());
-    }
+		MinecraftForge.EVENT_BUS.register(new SleepHandler());
+	}
 }
