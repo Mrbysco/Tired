f63e9d9145d179d60bbc54541137a753529eeabf
Mrbysco <Mrbysco@users.noreply.github.com>
2023-12-29 01:44:51 +0100

    Port to 1.20.4

--- src/main/java/com/mrbysco/tired/Tired.java
+++ src/main/java/com/mrbysco/tired/Tired.java
@@ -6,7 +6,6 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 
@@ -15,8 +14,7 @@ public class Tired {
 	public static final String MOD_ID = "tired";
 	public static final Logger LOGGER = LogUtils.getLogger();
 
-	public Tired() {
-		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+	public Tired(IEventBus eventBus) {
 		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, SleepSchedule.commonSpec);
 		eventBus.register(SleepSchedule.class);
 
